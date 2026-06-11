b477b6ac2ad75a1f60030dcd6146c979e4202354
Nierhoff <anders.nierhoff@gmail.com>
2026-06-11 14:20:32 +0200

    initial version

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+
+fn fib(n: u64) -> num::BigUint {
+    let mut a: num::BigUint = num::BigUint::ZERO;
+    let mut b: num::BigUint = num::BigUint::from(1u32);
+    let mut c: num::BigUint;
+    for _ in 2..n {
+        c = &a + &b;
+        a = b;
+        b = c;
+    }
+    b
+}
+
+fn main() {
+    let n = 100_000;
+    let start = std::time::Instant::now();
+    let result = fib(n);
+    println!("Fibonacci of {} is: {}", n, result);
+    let duration = start.elapsed();
+    println!("Time taken: {:?}", duration);
+}
